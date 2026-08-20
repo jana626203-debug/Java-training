d71125a16ec6646931e222ebe4c0c76cc388ab20
jana626203-debug <jana626203@gmail.com>
2026-08-20 22:55:12 +0530

    Change string value from 'jeevanprasath' to 'jana'

--- main6.java
+++ main6.java
@@ -1,7 +1,7 @@
 public class Main
 {
 	public static void main(String[] args){
-	    String a = "jeevanprasath";
+	    String a = "jana";
 	    int c=0;
 	    for(int i = 0;i < a.length();i++)
 	    {
