41433454211eba5b042e471b1d608a7c5324b4a5
jana626203-debug <jana626203@gmail.com>
2026-08-21 21:50:38 +0530

    Fix formatting issues in main5.java

--- main5.java
+++ main5.java
@@ -5,7 +5,7 @@ public class Main
 	    for(int i = 0;i < a.length()-1;i++)
 	    {
 	        for(int j = i+1;j < a.length();j++)
-	        {
+				{
 	            if(a.charAt(i) == a.charAt(j))
 	            {
 	                System.out.println(a.charAt(i));
